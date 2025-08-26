ba7fc2fdc61aa0a18f6c5da36f890aa1d19796c7
Gavarasana Saranya <intern1@greenfossil.com>
2025-08-26 16:09:07 +0800

    Allows bookmarking for news article

---
+++ Investment App Simulator/src/controllers/newsController.js
@@ -0,0 +1,43 @@
+const newsModel = require('../models/news');
+
+const { Parser } = require('json2csv');
+
+module.exports.getNewsController = function (req, res) {
+    const { category, minId } = req.query;
+
+    newsModel.getMarketNews(category, minId)
+        .then(newsList => {
+            return res.status(200).json(newsList);
+        })
+        .catch(error => {
+            console.error(error);
+
+            if (error.message === 'Category is required') {
+                return res.status(400).json({ error: error.message });
+            }
+
+            return res.status(500).json({ error: error.message });
+        });
+};
+
+
+module.exports.bookmarkNewsController = async function(req, res) {
+  const { userId, newsData } = req.body;
+
+  if (!userId || !newsData) {
+    return res.status(400).json({ error: "userId and newsData are required" });
+  }
+
+  try {
+    const result = await newsModel.bookmarkNews(userId, newsData);
+
+    if (!result.success) {
+      return res.status(400).json({ success: false, message: result.message });
+    }
+
+    return res.status(200).json({ success: true, bookmark: result.bookmark });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
---
+++ Investment App Simulator/src/routers/news.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const newsController = require('../controllers/newsController');
+const jwtMiddleware = require("../middlewares/jwtMiddleware");
+
+
+// router.post('/:id/view', chartsController.incrementCommentViewController);
+router.get('/news', newsController.getNewsController);
+
+// Bookmark news (only inserts into DB if bookmarked)
+router.post('/news/bookmark', newsController.bookmarkNewsController);
+
+module.exports = router;
