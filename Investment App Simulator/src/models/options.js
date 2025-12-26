ca13ff70ea1445edd10d8169f4cea83009deec1e
Gavarasana Saranya <intern1@greenfossil.com>
2025-12-26 11:17:13 +0800

    reverted

--- Investment App Simulator/src/models/options.js
+++ Investment App Simulator/src/models/options.js
@@ -4,8 +4,7 @@ const API_KEY= process.env.ALPACA_API_KEY;
 const API_SECRET= process.env.ALPACA_API_SECRET;
 
 const ALPHA_VANTAGE_API_KEY = process.env.ALPHA_VANTAGE_API_KEY;
-// const POLYGON_API_KEY = process.env.POLYGON_API_KEY;
-POLYGON_API_KEY = '6MfvDldYOniwkrACq6HAmmmx9Ee75PEC'
+const POLYGON_API_KEY = process.env.POLYGON_API_KEY;
 const prisma = require('./prismaClient'); // import the Prisma instance
 const fetch = require('node-fetch');
 
